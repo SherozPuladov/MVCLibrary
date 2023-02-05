6c2b3f48f205b3be2b9587132dac29334a32d906
Sheroz Puladov <108729229+SherozPuladov@users.noreply.github.com>
2023-02-05 22:01:52 +0500

    Add files via upload

---
+++ MVCLibrary/MVCLibrary/Migrations/20230110020830_AddedBookAuthor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBookAuthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CallNumber",
+                table: "Book",
+                newName: "CallNumber");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Book");
+
+            migrationBuilder.RenameColumn(
+                name: "CallNumber",
+                table: "Book",
+                newName: "Number");
+        }
+    }
+}
